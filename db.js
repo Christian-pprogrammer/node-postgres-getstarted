0a57d6eba809a9c986f6fc05152331562b291e82
MPANO Christian <mpanoc6@gmail.com>
2022-01-22 17:54:47 +0200

    added db configurations

---
+++ db.js
@@ -0,0 +1,10 @@
+const Pool = require('pg').Pool;
+const { HOSTNAME, PASSWORD, USERNAME, DB_NAME, DB_PORT } = process.env; 
+const pool = new Pool({
+    host: HOSTNAME,
+    password: PASSWORD,
+    user: USERNAME,
+    database: DB_NAME,
+    port: DB_PORT
+});
+module.exports.pool = pool;
