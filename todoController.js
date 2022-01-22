b69f13bd2ebefe2df120c6efe7e54452b4a7f303
MPANO Christian <mpanoc6@gmail.com>
2022-01-22 18:26:40 +0200

    added a function to create a todo

--- todoController.js
+++ todoController.js
@@ -12,3 +12,17 @@ exports.getTodos = async (req,res) => {
         })
     }
 }
+
+exports.createTodo = async (req,res) => {
+    try {
+        const newTodo = await pool.query(`INSERT INTO todo(id, title, description) VALUES(uuid.generate_v4(), ${req.body.title}, ${req.body.description}) RETURNING *`);
+        res.status(200).json({
+            newTodo: newTodo
+        })
+    }catch(err) {
+        res.status(400).json({
+          status: false,
+          error: err.message,
+        });
+    }
+}
