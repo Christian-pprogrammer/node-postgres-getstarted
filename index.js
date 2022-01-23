5e50d1650a2ad6fb7a35f1ebca9f01ff545db9da
MPANO Christian <mpanoc6@gmail.com>
2022-01-23 09:20:00 +0200

    added swagger doc definition

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config({ path: './.env' });
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 const router = require('./router');
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'Todo API',
+            description: 'Todo API to help people keep their todos organised',
+            contact: {
+                name: 'MPAN Christian'
+            },
+            servers: ['http://localhost:5000']
+        }
+    },
+    apis: ["./router.js"]
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 const app = express();
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/', router);
 app.listen(5000, () => {
     console.log('express app listening on port 5000');
